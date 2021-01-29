28a0d3f6f27f106cfe92f2fd7f306289e2b7c350
nammon72 <namnh.vst@gmail.com>
2021-01-29 09:22:10 +0700

    nothing

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var name = "Nguyễn Hoàng Nam";
+var age = 30;
+var sex = true;
+if (sex == true ) {
+    sex = "Male"
+} else {
+    sex = "Famale"
+}
+
+function hireMe() {
+    alert("Nguyễn Hoàng Nam, phone: 0981554491")
+}
