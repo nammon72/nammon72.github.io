cbc01a0e24c363a7949c4c14f29727b017a21d81
Nam Nguyen <nammon72@MacBookPro.local>
2021-03-03 17:29:37 +0700

    create login_form

---
+++ login_form/register.js
@@ -0,0 +1,25 @@
+function submitData () {
+	var nameTag = document.getElementById("name")
+	localStorage.setItem("name", nameTag.value)
+
+	var ageTag = document.getElementById("age")
+	localStorage.setItem("age", ageTag.value)
+
+	var emailTag = document.getElementById("email")
+	localStorage.setItem("email", mailTag.value)
+}
+
+function showData() {
+	name = localStorage.getItem("name")
+	age = localStorage.getItem("age")
+	email = localStorage.getItem("email")
+
+	document.getElementById("data").innerHTML +=
+	`
+		<tr>
+			<td>${name}</td>
+			<td>${age}</td>
+			<td>${email}</td>
+		</tr>
+	`
+}
