aaef81fd56f1f2ef4d5904672cf4815e77557572
Nam Nguyen <namnh.vst@gmail.com>
2022-04-20 17:37:29 +0700

    Build TodoList Apps

---
+++ React-Tutorial/tutorial-src/src/App.js
@@ -0,0 +1,9 @@
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <h3>Danh sách cần làm...</h3>
+  )
+}
+
+export default App;
---
+++ React-Tutorial/tutorial-src/src/components/TodoList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TodoList() {
+    return <h2>Day la Todo List</h2>;
+}
